01909f5ffa4434726cb3d436398abf8e06e315fe
Trevor Hartman <trevorhartman@gmail.com>
2013-12-03 10:30:53 -0700

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject utsusu "0.1.0-SNAPSHOT"
+(defproject utsusu "0.1.0"
   :description "A tool to transfer git repositories between GitHub and GitHub Enterprise"
   :url "https://github.com/devth/utsusu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main utsusu.core
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [me.raynes/conch "0.6.0"]
                  [tentacles "0.2.6"]])
