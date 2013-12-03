6af9abc26041172df69511e8d43b44ae9d8318c1
Trevor Hartman <trevorhartman@gmail.com>
2013-12-03 13:07:14 -0700

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utsusu "0.1.1"
+(defproject utsusu "0.1.2-SNAPSHOT"
   :description "A tool to transfer git repositories between GitHub and GitHub Enterprise"
   :url "https://github.com/devth/utsusu"
   :license {:name "Eclipse Public License"
